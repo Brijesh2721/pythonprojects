988671cc4c7d0b2a177f17ae0b9f0992e4552156
Brijesh Sharma <99115191+Brijesh2721@users.noreply.github.com>
2023-05-18 12:39:53 +0530

    Add files via upload

---
+++ VoiceGPT.py
@@ -0,0 +1,73 @@
+import openai
+import pyttsx3
+import speech_recognition as sr
+import time
+
+# Set OpenAI API key
+openai.api_key = "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"
+
+# Initialize the text-to-speech engine
+engine = pyttsx3.init()
+
+def transcribe_audio_to_text(filename):
+    recognizer = sr.Recognizer()
+    with sr.AudioFile(filename) as source:
+        audio = recognizer.record(source)
+    try:
+        return recognizer.recognize_google(audio)
+    except:
+        print('Skipping Unknown Error')
+
+def generate_response(prompt):
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=4000,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    return response.choices[0].text
+
+def speak_text(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def main():
+    while True:
+        # Wait for the user to say "rocky"
+        print("Say 'rocky' to start recording your question...")
+        with sr.Microphone() as source:
+            recognizer = sr.Recognizer()
+            audio = recognizer.listen(source)
+
+        try:
+            transcription = recognizer.recognize_google(audio)
+            if transcription.lower() == "rocky":
+                # Record audio
+                filename = "input.wav"
+                print("Say your question...")
+                with sr.Microphone() as source:
+                    recognizer = sr.Recognizer()
+                    source.pause_threshold = 1
+                    audio = recognizer.listen(source, phrase_time_limit=None, timeout=None)
+                    with open(filename, "wb") as f:
+                        f.write(audio.get_wav_data())
+
+                # Transcribe audio to text
+                text = transcribe_audio_to_text(filename)
+                if text:
+                    print(f"You said: {text}")
+
+                    # Generate responses using ChatGPT-3
+                    response = generate_response(text)
+                    print(f"ChatGPT-3 says: {response}")
+
+                    # Read response
+                    speak_text(response)
+
+        except Exception as e:
+            print("An error occurred: {}".format(e))
+
+if __name__ == "__main__":
+    main()
